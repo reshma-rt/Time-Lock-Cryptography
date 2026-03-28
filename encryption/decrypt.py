92e0d360e2e05fea7df6d68d5dd12e11d7a0d2a6
Reshma Banu <reshmabanu.a2023@vitstudent.ac.in>
2026-03-28 12:48:40 +0530

    Delete encryption directory

--- encryption/decrypt.py
+++
@@ -1,5 +0,0 @@
-from cryptography.fernet import Fernet
-
-def decrypt_message(encrypted, key):
-    cipher = Fernet(key)
-    return cipher.decrypt(encrypted).decode()
--- encryption/encrypt.py
+++
@@ -1,7 +0,0 @@
-from cryptography.fernet import Fernet
-
-def encrypt_message(message):
-    key = Fernet.generate_key()
-    cipher = Fernet(key)
-    encrypted = cipher.encrypt(message.encode())
-    return key, encrypted

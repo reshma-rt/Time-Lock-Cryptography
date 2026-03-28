689566263ee8689bace4f2c2fdfe5f6903baa303
Reshma Banu <reshmabanu.a2023@vitstudent.ac.in>
2026-03-28 12:48:50 +0530

    Delete secret_sharing directory

--- secret_sharing/reconstruct_key.py
+++
@@ -1,3 +0,0 @@
-def reconstruct_key(shares):
-    # Since all shares are same (simulation)
-    return shares[0]
--- secret_sharing/split_key.py
+++
@@ -1,4 +0,0 @@
-def split_key(key, total, required):
-    # Simple simulation (same key distributed)
-    shares = [key for _ in range(total)]
-    return shares

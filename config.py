4711fcd486a2581c8c6fab932af76c610245d138
Reshma Banu <reshmabanu.a2023@vitstudent.ac.in>
2026-03-28 12:49:26 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-# Set a fixed time manually (adjust to your current time + 1 minute)
-UNLOCK_TIME = datetime(2026, 3, 24, 16, 30, 0)
-
-REQUIRED_SHARES = 3
-VALID_LOCATION = "Chennai"
